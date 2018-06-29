36b64e48c2285001864506efd78b4cd27dbe14ce
unknown <sunnygupta2028@gmail.comgit config --global user.name sunny2028>
2018-06-29 20:07:13 +0530

    first commit

---
+++ src/main/java/com/example/dell/myapplication/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.dell.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+       /* Button button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Button Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+        Log.d("ActivityState", "OnCreate()");*/
+    }
+}
